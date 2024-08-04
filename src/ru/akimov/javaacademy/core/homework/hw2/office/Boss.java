f2458d712051f3dc35583719f8c0a29f23928ee0
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2024-08-04 16:28:59 +0300

    add hw2

---
+++ src/ru/akimov/javaacademy/core/homework/hw2/office/Boss.java
@@ -0,0 +1,13 @@
+package ru.akimov.javaacademy.core.homework.hw2.office;
+
+public class Boss {
+    String name;
+
+    public Boss(String name) {
+        this.name = name;
+    }
+
+    public void action(Manager manager) {
+        System.out.println("босс: " + manager.name + " быстрее!");
+    }
+}
